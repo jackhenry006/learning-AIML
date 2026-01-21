913b0772c8573af3b2afac0587a85f72c63f6c8d
Ankit Kumar Nayak <ankitkumarnayak006@gmail.com>
2026-01-22 00:08:03 +0530

    q2 done

--- ass5.py
+++ ass5.py
@@ -1,4 +1,5 @@
 # class question
+"""
 data=True
 line=1
 with open("sample.txt","r") as f:
@@ -10,6 +11,29 @@ with open("sample.txt","r") as f:
             break
 
         line+=1
+"""
 
+# q1
+"""
+with open("sample.txt","w") as f: 
+    f.write("ankit\nsunil\nabhishek\nswastik\nblesson")
 
-    
+with open("sample.txt","r") as f:
+    data=f.read()
+    print(data)
+"""
+
+#q2
+
+"""
+with open("sample.txt","a+")as f:
+    num=int(input("Enter the number of logs you want to enter : "))
+    for i in range(num):
+        data=input("Enter the log : ")
+        f.write(data)
+
+with open("sample.txt","r") as f:
+    value=f.read()
+    print(value)
+
+"""
